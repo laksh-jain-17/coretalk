3b23a6677ba7e2617f5ea8a85319b271ce0164a9
laksh-jain-17 <lakshjain1705@gmail.com>
2026-03-07 20:25:05 +0530

    Create OAuthCallback.vue
    New file for Google permission popup

---
+++ src/pages/OAuthCallback.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Authenticating...</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const hash = window.location.hash;
+    const params = new URLSearchParams(hash.replace('#', ''));
+    const token = params.get('access_token');
+    if (token && window.opener) {
+      window.opener.postMessage({ type: 'gmail-oauth-success', token }, '*');
+      window.close();
+    }
+  }
+}
+</script>
