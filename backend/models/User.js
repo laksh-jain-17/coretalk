27a9d6934d4ef5d8a1ff980a8d26e0e2b48c1c4e
Laksh Jain <lakshjain1705@gmail.com>
2026-03-10 09:41:56 +0530

    Update User.js
    Modifying the email attribute

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,12 +20,13 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
+    email: { type: String, required: true, unique: true, lowercase: true, index: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
     googleId: { type: String, unique: true, sparse: true }  // Add this line
 }, {
     timestamps: true
 });
+
 
 module.exports = mongoose.model('User', userSchema);
