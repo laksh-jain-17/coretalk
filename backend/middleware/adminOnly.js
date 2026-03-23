2cfb29c86d5d6390c7766a0f4e70f307c83d972d
Laksh Jain <lakshjain1705@gmail.com>
2026-03-23 17:29:45 +0530

    Create adminOnly.js
    NEW FILE

---
+++ backend/middleware/adminOnly.js
@@ -0,0 +1,8 @@
+function adminOnly(req, res, next) {
+  if (!req.user || !req.user.isAdmin) {
+    return res.status(403).json({ msg: 'Access denied. Admins only.' });
+  }
+  next();
+}
+
+module.exports = adminOnly;
