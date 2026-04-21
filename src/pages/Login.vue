60bb37fee5e490e2a0aee95896ef37b5463eb4b9
Laksh Jain <lakshjain1705@gmail.com>
2026-04-21 09:58:28 +0530

    Update Login.vue
    Version 3 more feature addressing.

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -14,7 +14,7 @@
             </div>
             <ol>
               <li>More security measures added.</li>
-              <li>User customization allowed.</li>
+              <li>File attachment in chatbox.</li>
               <li>OTP-based authentication is enabled.</li>
               <li>FAQ is available.</li>
               <li>Sign in as guest account is provided.</li>
