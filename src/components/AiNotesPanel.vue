88a255425df7f8ce1bf9ef47e2442ce49fe51127
Laksh Jain <lakshjain1705@gmail.com>
2026-05-14 21:20:29 +0530

    Update AiNotesPanel.vue

--- src/components/AiNotesPanel.vue
+++ src/components/AiNotesPanel.vue
@@ -292,7 +292,7 @@ export default {
 
     async download() {
       const { Document, Packer, Paragraph, TextRun, HeadingLevel } = await import(
-        'https://cdn.jsdelivr.net/npm/docx@8.5.0/build/index.js'
+        'https://esm.sh/docx@8.5.0'
       );
 
       const children = [];
